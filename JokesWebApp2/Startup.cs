a2be8bfa264ff08b5885cd4f3ff9e627d814fc4f
Dhanu <dhanushan.info@gmail.com>
2023-06-25 20:59:35 +0530

    Add project files.

---
+++ JokesWebApp2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JokesWebApp2.Startup))]
+namespace JokesWebApp2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
